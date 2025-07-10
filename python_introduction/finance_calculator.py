9bf3231326645a9a71dce293c9994f712b1117be
moalsafi9 <modata32@gmail.com>
2025-07-10 17:58:16 +0300

    uploading all tasks files

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+income = float(input("Enter your monthly income: "))
+expenses = float(input("Enter your total monthly expenses: "))
+
+savings = income - expenses
+Projected_Savings = savings * 12 + (savings * 12 * 0.05)
+
+print("Your monthly savings are", savings)
+print("Projected savings after one year, with interest, is:", Projected_Savings)
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,3 @@
+age = int(input("How old are you?"))
+future_age = 27 + age
+print("In 2050, you will be", future_age, "years old.")
