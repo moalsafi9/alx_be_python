a295d01edf555629693da2cbd1edd176167e6347
moalsafi9 <modata32@gmail.com>
2025-07-10 17:32:33 +0300

    task0 files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+sub = number1 + number2
+mul = number1 + number2
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtracting of", number1, "and", number2, "is", sub)
+print("Multiplication of", number1, "and", number2, "is", mul)
