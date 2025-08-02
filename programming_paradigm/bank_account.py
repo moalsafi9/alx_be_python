d4c472e08e581753704258140b4debbb81b29899
moalsafi9 <modata32@gmail.com>
2025-08-03 01:52:53 +0300

    Uploading tasks

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,16 @@
 class BankAccount:
     def __init__(self, account_balance=0) -> None:
         self.account_balance = account_balance
-        
-    
+
     def deposit(self, amount):
-        self.account_balance = self.account_balance + amount
-    
+        self.account_balance += amount
+
     def withdraw(self, amount):
         if self.account_balance < amount:
             return False
         else:
-            self.account_balance = self.account_balance - amount
+            self.account_balance -= amount
             return True
-        
+
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
